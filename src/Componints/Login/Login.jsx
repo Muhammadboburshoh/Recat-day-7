d9c8b9332d55cd9f916ac2f52105c7adf53ab4d3
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-01-07 15:04:43 +0500

    site login part finished

--- src/Componints/Login/Login.jsx
+++ src/Componints/Login/Login.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
+
+import "./Login.css"
+
 function Login () {
+
+  const [ userName, setUserName ] = useState("")
+  const [ userPassword, setUserPassword ] = useState("")
+
+  const [ disabled, setDisabled ] = useState(true)
+  useEffect(() => {
+
+    if(userName.length > 3 && userPassword.length > 5) {
+      setDisabled(false)
+    }
+    else {
+      setDisabled(true)
+    }
+  }, [userName, userPassword])
 
   return (
     <>
-      Login
+      <input onChange={evt => { setUserName(evt.target.value)}} type="text" placeholder="John"/>
+
+      <input onChange={evt => { setUserPassword(evt.target.value)}} type="password" placeholder="password" />
+
+      <button disabled={disabled}>Submit</button>
     </>
   )
 }
